145582d734f0facf43a11a68253fb7df4308ef78
GITZMBE <lucasfilip@hotmail.se>
2026-05-25 19:19:49 +0200

    fix: make TrailerLink longer and inner text not wrapping

--- src/components/connectors/Trailer/TrailerLink.tsx
+++ src/components/connectors/Trailer/TrailerLink.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 export const TrailerLink = ({ videoKey }: Props) => {
   return (
-    <a href={`https://www.youtube.com/watch?v=${videoKey}`} target="_blank" className="flex items-center gap-2 w-full p-4 rounded-lg border border-secondary">
+    <a href={`https://www.youtube.com/watch?v=${videoKey}`} target="_blank" className="flex items-center gap-2 w-full min-w-40 p-4 rounded-lg border border-secondary">
       <FaCirclePlay className="text-3xl" />
-      <p className="text-sm">Watch trailer</p>
+      <p className="text-sm text-nowrap">Watch trailer</p>
     </a>
   )
 };
